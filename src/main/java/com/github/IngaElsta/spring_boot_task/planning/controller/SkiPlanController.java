7d9a5a663ff8a999dce4e823918bc8951218cb85
IngaElsta <inga.elsta@gmail.com>
2022-04-06 15:56:04 +0300

    Cleaning up controller

--- src/main/java/com/github/IngaElsta/spring_boot_task/planning/controller/SkiPlanController.java
+++ src/main/java/com/github/IngaElsta/spring_boot_task/planning/controller/SkiPlanController.java
@@ -4,19 +4,17 @@ import com.github.IngaElsta.spring_boot_task.planning.service.SkiPlanService;
 import com.github.IngaElsta.spring_boot_task.planning.domain.SkiLocation;
 import com.github.IngaElsta.spring_boot_task.weather.domain.WeatherConditions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.server.ResponseStatusException;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import java.time.LocalDate;
 import java.util.Map;
 
-@Slf4j
 @RestController
 @Validated
 @RequestMapping ("api/v1/ski-planner")
@@ -37,13 +35,7 @@ public class SkiPlanController {
             @RequestParam (value = "lon", required = false, defaultValue = "24.11")
             @Min(-180) @Max(180)
                     Double lon) {
-        try {
-            return skiPlanService.getWeather(new SkiLocation(lat, lon));
-        } catch (HttpClientErrorException e) {
-            log.error("GetWeather: Invalid values were passed for location - lat {}, lon {}", lat, lon);
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "Invalid values were passed for location");
-        }
+        return skiPlanService.getWeather(new SkiLocation(lat, lon));
     }
 
 

e14a1a5641194cfdad852e781b47a564f5f92671
IngaElsta <inga.elsta@gmail.com>
2022-05-05 19:17:16 +0300

    Adding todo for test renaming

--- src/test/java/com/github/ingaelsta/outdooractivityplanner/SpringBootTaskApplicationTests.java
+++ src/test/java/com/github/ingaelsta/outdooractivityplanner/SpringBootTaskApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootTaskApplicationTests {
 
+	//todo: mass rename for tests when acquiring info about needed naming convention
 	@Test
 	void contextLoads() {
 	}

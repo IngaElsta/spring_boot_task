53582e75fdccb515a29e1a8588685d5d60d23ae7
IngaElsta <inga.elsta@gmail.com>
2022-05-19 20:07:16 +0300

    Returning back from URIBuilder to URITemplate in OWMDataService

--- src/main/java/com/github/ingaelsta/outdooractivityplanner/weather/service/OWMDataService.java
+++ src/main/java/com/github/ingaelsta/outdooractivityplanner/weather/service/OWMDataService.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriTemplate;
 
 import java.net.URI;
 import java.time.Duration;
@@ -25,43 +25,32 @@ import java.util.Map;
 @Slf4j
 @Component
 public class OWMDataService implements WeatherDataService {
-    private final String owmOneApiUrl = "https://api.openweathermap.org/data/2.5";
     private final ObjectMapper objectMapper;
     private final OWMConfiguration owmConfiguration;
-
     private final WebClient webClient;
 
     public OWMDataService(OWMConfiguration owmConfiguration,
                           OWMObjectMapperConfiguration OWMobjectMapperConfiguration) {
         this.owmConfiguration = owmConfiguration;
         this.objectMapper = OWMobjectMapperConfiguration.getObjectMapper();
-        this.webClient = WebClient.create(owmOneApiUrl);
+        this.webClient = WebClient.create();
     }
 
     public Map<LocalDate, WeatherConditions> retrieveWeather (Location location) {
-        //todo: might try again using the original string from properties file
         //todo: works but seems much slower (15s???)... commit now, investigate later
         //todo: add circuit breaker
+        URI owmURI = new UriTemplate(owmConfiguration.getOneApiUrl())
+                .expand(location.getLatitude(), location.getLongitude(),
+                        owmConfiguration.getAuthToken());
         String response = webClient.get()
-                .uri(uriBuilder -> buildWithParams(location, uriBuilder))
+                .uri(owmURI)
                 .retrieve()
                 .onStatus(HttpStatus::isError, throwable -> {
                     throw new OWMDataException("Failed to retrieve weather data");
                 })
                 .bodyToMono(String.class)
-                .block(Duration.of(60000, ChronoUnit.MILLIS));
+                .block(Duration.of(30000, ChronoUnit.MILLIS));
         return processWeatherData(response, objectMapper);
-    }
-
-    private URI buildWithParams(Location location, UriBuilder uriBuilder) {
-        return uriBuilder
-                .path("/onecall")
-                .queryParam("lat", location.getLatitude())
-                .queryParam("lon", location.getLongitude())
-                .queryParam("exclude", "minutely,hourly,current")
-                .queryParam("appid", owmConfiguration.getAuthToken())
-                .queryParam("units", "metric")
-                .build();
     }
 
     //todo: probably integrate it back into retrieve weather method
@@ -69,6 +58,7 @@ public class OWMDataService implements WeatherDataService {
             String weatherJson,
             ObjectMapper objectMapper) {
         try {
+            //todo: look into "unchecked assignment" warning
             return objectMapper.readValue(weatherJson, Map.class);
         } catch (JsonProcessingException e) {
             log.error("processWeatherData: Failed to process weather data {}", weatherJson);
